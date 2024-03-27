3315c4794f6b3419b983677f0521ce6186d9a652
Malik-Saqlain <dmsaqlain.ms@gmail.com>
2024-03-27 19:34:11 +0500

    conversion of dataTypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = "33"
+
+// console.log (typeof score);
+// console.log (typeof (score));
+
+ let valueNumber = Number(score);
+//  console.log(valueNumber);
+
+
+//  "33" => 33
+//  "33xyz" => NaN
+//  true => 1;  fa  lse => 0
+
+let isLoggedIn = "0"
+let booleanIsLoggedIn = Boolean (isLoggedIn);
+// console.log (booleanIsLoggedIn);
+// console.log(typeof booleanIsLoggedIn);
+
+
+//  1 => true; 0 => false
+//  "" => false
+//  "Malik" => true
+
+
+let someNumber = 55
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log (typeof stringNumber);
