a3e6e177f544e363cfc27277c118df8728dc682e
Malik-Saqlain <dmsaqlain.ms@gmail.com>
2024-07-12 01:11:27 +0500

    comparisions in JS

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,23 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+console.log("2" < 1);
+console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined >= 0);
+console.log(undefined < 0);
+
+//  NOTE: avoid these type of comparision as they are confusing.
+
+// ===
+console.log("2" === 2);
